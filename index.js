dffbbd77ae4d1dd15edcc9a5ae93df3ae3e03c60
Yeohoon <yhoon.yoon@gmail.com>
2022-01-17 22:44:43 -0500

    commit tree f1924389ea377fbebe0e97c2f18d3b41f5a077d0

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-function receivesAFunction(index){
-  return index();
+// function receivesAFunction(index){
+//   return index();
+// }
+
+
+// function returnsANamedFunction(){
+//   return returnsANamedFunction;
+// };
+
+// function returnsAnAnonymousFunction(){
+//   return function(){return returnsAnAnonymousFunction};
+// }
+
+
+
+
+
+
+
+
+
+
+
+function receivesAFunction(cb){
+  return cb();
 }
-let spy = function() {return 1+1};
+receivesAFunction(()=>console.log("hi"))
 
 function returnsANamedFunction(){
-  return returnsANamedFunction;
-};
+  return function named(){return named;}
+}
 
 function returnsAnAnonymousFunction(){
-  return function(){return returnsAnAnonymousFunction};
-}
+  return function(){console.log('yeohoon')}}
